cd68786865699bbc596f9ce9c04dc57c8ec358c6
AnonymousinJP <wp360sfs@gmail.com>
2024-04-05 21:45:02 +0900

    Na2_first_commit

---
+++ BashoToDo/BashoToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BashoToDoApp.swift
+//  BashoToDo
+//
+//  Created by 夏純心 on 2024/03/25.
+//
+
+import SwiftUI
+
+@main
+struct BashoToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
