b43e5d39c02693cc8d9f1d622c6899a828319856
orukahairuka <sakuraierika03301109@gmail.com>
2024-04-05 22:29:02 +0900

    wip

---
+++ BashoToDo/MapScreen.swift
@@ -0,0 +1,18 @@
+//
+//  MapScreen.swift
+//  BashoToDo
+//
+//  Created by 櫻井絵理香 on 2024/04/05.
+//
+
+import SwiftUI
+
+struct MapScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapScreen()
+}
---
+++ BashoToDo/MemoEditScreen.swift
@@ -0,0 +1,18 @@
+//
+//  MemoEditScreen.swift
+//  BashoToDo
+//
+//  Created by 櫻井絵理香 on 2024/04/05.
+//
+
+import SwiftUI
+
+struct MemoEditScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MemoEditScreen()
+}
